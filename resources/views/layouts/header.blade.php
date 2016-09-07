6d4340f54abe6d688332ca83fd729d87820cc5c9
Jason Herndon <jasonherndon86@gmail.com>
2016-09-07 00:28:39 -0600

    Updated Auth name used in header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,7 +54,7 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
+                                {{ Auth::user()->first }} {{ Auth::user()->last }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
