a5c60c256b5d5fe3d2dbeb328d2c38bbb1ee2f2b
Jason Herndon <jasonherndon86@gmail.com>
2016-10-11 15:37:24 -0600

    View work

---
+++ resources/views/admin/site/posts/partials/menu.blade.php
@@ -0,0 +1,6 @@
+        <div class="sub-menu">
+            <ul class="breadcrumb">
+                <li><a href="{{ route('admin.posts.index') }}">All Posts</a></li>
+                <li><a href="{{ route('admin.posts.create') }}">Create New Post</a></li>
+            </ul>
+        </div>
